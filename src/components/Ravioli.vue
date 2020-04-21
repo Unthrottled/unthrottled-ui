5b2837ad6f035d25fa9ceb4094f3eff6cb4709c8
DATBOI <alexsimons9999@gmail.com>
2020-04-21 06:11:19 -0500

    Lots of interesting stuffs

--- src/components/Ravioli.vue
+++ src/components/Ravioli.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <div>Fam {{ ravioli }}</div>
+    <div>
+      {{ ravioli }}
+      <button v-on:click="giveMeTheFormuoli">give me the formuoli</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue} from "vue-property-decorator";
+import { Component, Prop, Vue, Emit } from "vue-property-decorator";
 
 @Component
 export default class Ravioli extends Vue {
   @Prop() private ravioli!: string;
+
+  @Emit("formuoli")
+  private giveMeTheFormuoli() {
+    //
+  }
 }
 </script>
 
--- src/views/Fam.vue
+++ src/views/Fam.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="fam-container">
-    <Ravioli ravioli="ravioli ravioli give me the formuoli" />
-    <button v-on:click="updateImage">Cycle Gril</button>
+    <p>{{ holderValue }}</p>
+    <h2 style="color: #2c3e50" v-if="vewyCheeky">Vewy Cheeky</h2>
+    <label>
+      <input v-model="userInput" type="text" />
+    </label>
+    <Ravioli :ravioli="userInput" @formuoli="updateImage" />
+    <h3>{{ displayGril.toUpperCase() }}</h3>
+    <h3 style="color: red" v-if="displayGril === 'ryuko'">Best Waifu</h3>
     <div :style="{ backgroundImage: gril() }" class="fam"></div>
   </div>
 </template>
@@ -24,20 +30,47 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Component } from "vue-property-decorator";
+import { Component, Model, Watch } from "vue-property-decorator";
 import Ravioli from "@/components/Ravioli.vue";
 
 @Component({
   components: { Ravioli }
 })
 export default class Fam extends Vue {
-  private grils = ["miku", "rem", "ibuki", "emilia_light", "emilia_dark"];
+  private grils = [
+    "miku",
+    "ryuko",
+    "satsuki",
+    "megumin",
+    "rem",
+    "ibuki",
+    "emilia_light",
+    "emilia_dark"
+  ];
   private currentGril = 0;
 
+  private vewyCheeky = false;
+  private holderValue = "";
+
+  $mount(elementOrSelector?: Element | string, hydrating?: boolean): this {
+    this.holderValue = this.displayGril;
+    return super.$mount(elementOrSelector, hydrating);
+  }
+
+  get displayGril(): string {
+    return this.grils[this.currentGril];
+  }
+
+  @Model()
+  userInput = "";
+
+  @Watch("userInput")
+  private userInputChanged(userInput: string) {
+    this.vewyCheeky = userInput.toUpperCase() === "RAVIOLI RAVIOLI";
+  }
+
   gril(): string {
-    return `url("https://doki.assets.unthrottled.io/backgrounds/${
-      this.grils[this.currentGril]
-    }.png")`;
+    return `url("https://doki.assets.unthrottled.io/backgrounds/${this.displayGril}.png")`;
   }
   updateImage() {
     this.currentGril = (this.currentGril + 1) % this.grils.length;
