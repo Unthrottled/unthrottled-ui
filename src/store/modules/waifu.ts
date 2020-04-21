b3b24787e4e563aac584308715fa05ed52f2dace
DATBOI <alexsimons9999@gmail.com>
2020-04-21 18:13:08 -0500

    Wafiu Module

---
+++ src/store/modules/waifu.ts
@@ -0,0 +1,25 @@
+import { Action, Module, Mutation, VuexModule } from "vuex-module-decorators";
+
+export enum Wafius {
+  MIKU = "miku",
+  RYUKO = "ryuko",
+  SATSUKI = "satsuki",
+  MEGUMIN = "megumin",
+  REM = "rem",
+  IBUKI = "ibuki",
+  EMILIA_LIGHT = "emilia_light",
+  EMILIA_DARK = "emilia_dark"
+}
+
+@Module({ namespaced: true, name: "Waifu" })
+export default class Waifu extends VuexModule {
+  public currentWaifu: Wafius = Wafius.RYUKO;
+  @Mutation
+  public setWaifu(newWaifu: Wafius): void {
+    this.currentWaifu = newWaifu;
+  }
+  @Action
+  public updateWaifu(newWaifu: Wafius): void {
+    this.context.commit("setWaifu", newWaifu);
+  }
+}
--- src/views/Fam.vue
+++ src/views/Fam.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="fam-container">
-    <p>{{ holderValue }}</p>
     <h2 style="color: #2c3e50" v-if="vewyCheeky">Vewy Cheeky</h2>
     <label>
-      <input v-model="userInput" type="text" />
+      <input type="text" v-model="userInput" />
     </label>
     <Ravioli :ravioli="userInput" @formuoli="updateImage" />
     <h3>{{ displayGril.toUpperCase() }}</h3>
+    <h3>Current Waifu: {{ currentWaifu }}</h3>
     <h3 style="color: red" v-if="displayGril === 'ryuko'">Best Waifu</h3>
     <div :style="{ backgroundImage: gril() }" class="fam"></div>
   </div>
@@ -16,6 +16,7 @@
 .fam-container {
   height: 100%;
 }
+
 .fam {
   background-size: cover;
   content: "";
@@ -30,50 +31,49 @@
 
 <script lang="ts">
 import Vue from "vue";
-import { Component, Model, Watch } from "vue-property-decorator";
+import { Component, Watch } from "vue-property-decorator";
 import Ravioli from "@/components/Ravioli.vue";
+import { Wafius } from "@/store/modules/waifu";
+import { namespace } from "vuex-class";
+
+const Waifu = namespace("Waifu");
+
+const grils = [
+  Wafius.MIKU,
+  Wafius.RYUKO,
+  Wafius.SATSUKI,
+  Wafius.MEGUMIN,
+  Wafius.REM,
+  Wafius.IBUKI,
+  Wafius.EMILIA_LIGHT,
+  Wafius.EMILIA_DARK
+];
 
 @Component({
   components: { Ravioli }
 })
 export default class Fam extends Vue {
-  private grils = [
-    "miku",
-    "ryuko",
-    "satsuki",
-    "megumin",
-    "rem",
-    "ibuki",
-    "emilia_light",
-    "emilia_dark"
-  ];
+  userInput = "";
+  @Waifu.State
+  public currentWaifu!: Wafius;
   private currentGril = 0;
-
   private vewyCheeky = false;
-  private holderValue = "";
-
-  $mount(elementOrSelector?: Element | string, hydrating?: boolean): this {
-    this.holderValue = this.displayGril;
-    return super.$mount(elementOrSelector, hydrating);
-  }
 
   get displayGril(): string {
-    return this.grils[this.currentGril];
-  }
-
-  @Model()
-  userInput = "";
-
-  @Watch("userInput")
-  private userInputChanged(userInput: string) {
-    this.vewyCheeky = userInput.toUpperCase() === "RAVIOLI RAVIOLI";
+    return grils[this.currentGril];
   }
 
   gril(): string {
     return `url("https://doki.assets.unthrottled.io/backgrounds/${this.displayGril}.png")`;
   }
+
   updateImage() {
-    this.currentGril = (this.currentGril + 1) % this.grils.length;
+    this.currentGril = (this.currentGril + 1) % grils.length;
+  }
+
+  @Watch("userInput")
+  private userInputChanged(userInput: string) {
+    this.vewyCheeky = userInput.toUpperCase() === "RAVIOLI RAVIOLI";
   }
 }
 </script>
