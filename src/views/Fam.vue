fd4b80f952579574a51411d830f0af298c3b1489
DATBOI <alexsimons9999@gmail.com>
2020-04-21 05:27:07 -0500

    Grils, grils, grils

--- src/views/Fam.vue
+++ src/views/Fam.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="fam-container">
     <Ravioli ravioli="ravioli ravioli give me the formuoli" />
-    <div class="fam"></div>
+    <button v-on:click="updateImage">Cycle Gril</button>
+    <div :style="{ backgroundImage: gril() }" class="fam"></div>
   </div>
 </template>
 
@@ -15,8 +16,8 @@
   z-index: 99999;
   width: 100%;
   height: 100%;
-  background: url("https://doki.assets.unthrottled.io/backgrounds/miku.png")
-    no-repeat center;
+  background-position: center;
+  background-repeat: no-repeat;
   opacity: 1;
 }
 </style>
@@ -29,5 +30,17 @@ import Ravioli from "@/components/Ravioli.vue";
 @Component({
   components: { Ravioli }
 })
-export default class Fam extends Vue {}
+export default class Fam extends Vue {
+  private grils = ["miku", "rem", "ibuki", "emilia_light", "emilia_dark"];
+  private currentGril = 0;
+
+  gril(): string {
+    return `url("https://doki.assets.unthrottled.io/backgrounds/${
+      this.grils[this.currentGril]
+    }.png")`;
+  }
+  updateImage() {
+    this.currentGril = (this.currentGril + 1) % this.grils.length;
+  }
+}
 </script>
