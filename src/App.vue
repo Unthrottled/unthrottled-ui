aa984d438b439298c21c0f23cb5225e96660f22a
DATBOI <alexsimons9999@gmail.com>
2020-04-24 04:55:29 -0500

    This should work, but it don't

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="iAmRoot">
     <Navigation />
-    <div class="main">
+    <div class="main" id="main">
+      <canvas
+        id="backgroundImage"
+        :width="backgroundWidth"
+        :height="backgroundHeight"
+      ></canvas>
       <div id="app">
         <transition mode="out-in" name="component-fade">
           <router-view />
@@ -22,6 +27,11 @@ body {
 }
 
 .main {
+  height: 100%;
+}
+
+#backgroundImage {
+  position: absolute;
 }
 
 #app {
@@ -49,5 +59,56 @@ import { Component } from "vue-property-decorator";
 @Component({
   components: { Navigation }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private backgroundWidth = 69;
+  private backgroundHeight = 69;
+
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener("resize", this.onResize);
+    });
+    this.updateSize();
+    this.drawBackground();
+  }
+
+  private updateSize() {
+    const mainCanvas = document.getElementById("main");
+    if (mainCanvas) {
+      const boundingRect = mainCanvas.getBoundingClientRect();
+      this.backgroundHeight = boundingRect.height;
+      this.backgroundWidth = boundingRect.width;
+    }
+  }
+
+  private drawBackground() {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const backgroundCanvas: any = document.getElementById("backgroundImage")!!;
+    const ctx: CanvasRenderingContext2D = backgroundCanvas.getContext("2d")!!;
+    console.log("drawing", this.backgroundWidth, this.backgroundHeight);
+    ctx.clearRect(0, 0, this.backgroundWidth, this.backgroundHeight);
+    ctx.beginPath();
+    ctx.moveTo(0, this.backgroundHeight);
+    ctx.quadraticCurveTo(
+      this.backgroundWidth / 1.5,
+      this.backgroundHeight,
+      this.backgroundWidth,
+      0
+    );
+    ctx.lineTo(this.backgroundWidth, this.backgroundHeight);
+    ctx.fillStyle = "#262c32";
+    ctx.strokeStyle = "#262c32";
+    ctx.fill();
+    ctx.closePath();
+    ctx.stroke();
+  }
+
+  beforeDestroy() {
+    window.removeEventListener("resize", this.onResize);
+  }
+
+  onResize() {
+    this.updateSize();
+    this.drawBackground();
+  }
+}
 </script>
