11bf186e41ff1af8328d8a9d658e00633630cd6b
DATBOI <alexsimons9999@gmail.com>
2020-05-01 11:32:35 -0500

    Thats about all I have to say about that

--- src/views/More.vue
+++ src/views/More.vue
@@ -7,10 +7,10 @@
       <div>
         <p>
           If anything needs to be done, go all in or do not do anything at all.
-          There is no point in doing something half way. It's wasted effort that
-          could have been directed to what really matters. We are wired to think
-          we can do many things at once. However, we are really bad at something
-          that makes us feel good about ourselves.
+          There is no point in doing something half way. It is wasted effort
+          that could have been directed to what really matters. We are wired to
+          think we can do many things at once. However, we are really bad at
+          something that makes us feel good about ourselves.
         </p>
         <p>
           When you: eliminate all distractions, only focus on the task at hand,
@@ -19,15 +19,32 @@
         </p>
         <p>
           The only thing that it will cost you is your time and effort. You have
-          the ability to increase your capacity for effort, but you cannot get
-          more time. Yes you can increase your lifespan to a certain extent by
-          maintain good health. However, pales in comparison to how much you can
-          increase other things in you life.
+          the ability to increase your capacity to expend effort, but you cannot
+          get more time. Yes, you can increase your lifespan to a certain extent
+          by maintain good health. However, pales in comparison to how much you
+          can increase other things in you life.
         </p>
         <p>
           I have dedicated my time and effort to achieving things I never
-          thought possible.
+          thought possible. This in turn has made me really good at the things I
+          deem valuable. I am also aware that almost every other person also
+          thinks that they are greatest at what they do. So my standpoint is not
+          unique. However, what set me apart from others my capacity to finish
+          really difficult things. Tasks that take an amazing amount of time,
+          effort, and persistence.
         </p>
+        <p>
+          If you want to see what I am talking about. Take a look at how I've
+          spent my time via my SOGoS history dashboard. For fun, choose a random
+          day in my past and just look at what an average day for me contains.
+        </p>
+        <a
+          class="dashboardLink"
+          href="https://sogos.unthrottled.io/dashboard/alex"
+          target="_blank"
+        >
+          https://sogos.unthrottled.io/dashboard/alex
+        </a>
       </div>
     </div>
   </Layout>
@@ -39,6 +56,12 @@
   margin-top: 2rem;
   font-weight: 300;
   color: #fff;
+}
+
+.dashboardLink {
+  font-size: 1.5rem;
+  text-decoration: none;
+  color: #e8ad4f;
 }
 
 .more {
